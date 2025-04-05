e8209c87899f25933dc4cde313a96447338c2e0e
ras-24 <siregar.riyando@gmail.com>
2025-04-05 09:51:29 +0800

    Changing ERC721 to ERC721Enumerable

--- contracts/BooknifyNFT_ERC721.sol
+++ contracts/BooknifyNFT_ERC721.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.8.0/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.8.0/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.8.0/contracts/access/Ownable.sol";
 
-contract BooknifyNFT is ERC721, Ownable {
+contract BooknifyNFT is ERC721Enumerable, Ownable {
     uint256 public TokenId;
     string private baseURI;
 
